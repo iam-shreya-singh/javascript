bdbae55daecd10e793baaa05ab1c5f52fbf82fd2
Shreya Singh <shreyasinghin24@gmail.com>
2024-10-26 12:58:27 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 115342
+let accountEmail = "shreya@google.com"
+var accountPassword = "12345"
+accountCity = "Ranchi"
+let accountState;
+
+//console.log(accountCity)
+
+console.table([accountId, accountEmail,accountPassword, accountCity,accountState])
